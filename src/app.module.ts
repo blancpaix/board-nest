b3340d603166c90f6db169a0dc2afc688fcb83ad
blancpaix <blancpaix@gmail.com>
2023-09-19 00:36:00 +0900

    FEATURE | add Mysql database with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { UsersModule } from './users/users.module';
 import { BoardsModule } from './boards/boards.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, BoardsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    BoardsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
